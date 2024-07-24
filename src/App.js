32c047ab5e8945a8573a8f60f87aeca5378a11a0
ameybahugune <asd>
2024-07-24 16:45:51 +0530

    create a Dreak mode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Form from './components/Form';
-
+import About from './components/About'
 function App() {
   return (
   <>
    <Navbar about="About" title="textUtile"/>
    <div className="container">
-   <Form title="Enter the text to analyze below"/>
+   {/* <Form title="Enter the text to analyze below"/> */}
+   <div className='my-4'>
+   <About />
+   </div>
    </div>
   </>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,23 +3,39 @@ import React , {useState}from 'react'
 export default function Form(props) {
 const [text,textuse]=useState("Enter a text")
 const click=()=>{
-  console.log("Uppercreate was Clivked"+ text)
+  // console.log("Uppercreate was Clivked"+ text)
   const newtext=text.toUpperCase();
   textuse(newtext);
 }
 const oncl=(even)=>{
-console.log("user a checking")
+// console.log("user a checking")
 textuse(even.target.value);
 }
 
+const LowerCase=()=>{
+  let newtext=text.toLocaleLowerCase();
+  textuse(newtext);
+}
+
   return (
-    <div> 
+    <>
+    <div className='container'> 
     <div className="mb-3">
     <label for="myBox" className="form-label my-3 ">{props.title}</label>
     <textarea className="form-control" id="exampleFormControlTextarea1"  onChange={oncl} value={text} rows="10" ></textarea>  
-    <button className="btn btn-primary my-2" onClick={click }>save</button>
+    <button className="btn btn-primary my-2" onClick={click}>Uppercreate</button>
+    <button className='btn btn-primary mx-3' onClick={LowerCase}>LowerCase</button>
     </div>
     </div>
+    <div>
+      <div className="container">
+        <h1>Your text summary</h1>
+        <p>{0.008 * text.split("").length}Minutest read</p>
+        <p>{text.split(" ").length} word ,{text.length} Chcharacter</p>        
+        <p>{text}</p>
+      </div>
+    </div>
+    </>
   )
 }
 
